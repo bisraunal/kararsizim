083d5891e23bbc0ea5e2a057946b8811a5ac1c7c
bisraunal <157958063+bisraunal@users.noreply.github.com>
2026-09-24 16:42:47 +0300

    ci: configure vercel.json and build_files.sh for Vercel deployment

---
+++ build_files.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+echo "Building project packages..."
+python3 -m pip install -r requirements.txt
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput --clear
